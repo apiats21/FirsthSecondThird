3a0cbfa2f12fae4e451c844f238c463414102ec4
apiats21 <pi134@hotmail.com>
2021-10-12 06:10:53 -0500

    FirstSecondThird with Semaphore and lambda

--- src/main/java/com/apiats21/Main.java
+++ src/main/java/com/apiats21/Main.java
@@ -1,10 +1,13 @@
 package main.java.com.apiats21;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         Foo foo = new Foo();
-        new Third(foo);
-        new Second(foo);
-        new First(foo);
+
+        new Thread( () -> { foo.first(); }).start();
+        new Thread( () -> { foo.second(); }).start();
+        new Thread( () -> { foo.third(); }).start();
+
+
     }
 }
